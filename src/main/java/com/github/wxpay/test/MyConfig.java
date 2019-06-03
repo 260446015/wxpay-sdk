96059760e487e6b29f9cb39b7e20cf82a2841416
尹大伟 <260446015@qq.com>
2019-06-03 23:15:14 +0800

    Initial commit

---
+++ src/main/java/com/github/wxpay/test/MyConfig.java
@@ -0,0 +1,12 @@
+package com.github.wxpay.test;
+
+import com.github.wxpay.sdk.WXPayConfig;
+
+/**
+ * @author yindwe@yonyou.com
+ * @Date 2019/6/3 0003 23:01
+ * @Description
+ */
+public class MyConfig extends WXPayConfig {
+
+}
